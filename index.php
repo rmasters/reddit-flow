345b781eccc10b465a157c648ccaf80850becae5
phalt <Paul@.(none)>
2010-11-26 23:18:47 +0000

    Modified index.php added image and styles.css

--- index.php
+++ index.php
@@ -3,8 +3,28 @@
 <head>
   <title>Watching the reddit flow</title>
   <script type="text/javascript" src="js/flow.js"></script>
+  <link href="styles.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-
+<div id="header">
+<h1>Reddit flow<img src="redditguy.jpg"></h1>
+</div>
+<div id="page">
+	<div id="content">
+	<!--Dynamic content goes here, list shown is a place holder-->
+		<?php
+		print "<ul type=none>";
+		for($no = 1; $no < 25; $no++){
+		print "<li>";
+		print "<a href='#'>Reddit link</a>";
+		print "</li>";
+		}
+		print "</ul>";
+		?>
+	</div>
+</div>
+<div id="footer">
+Created by <a href="http://www.rossmasters.com/">Ross Masters</a> and <a href="http://www.phalt.co.uk">Paul Hallett</a>
+</div>
 </body>
 </html>
